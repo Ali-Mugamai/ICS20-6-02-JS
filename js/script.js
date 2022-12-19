24a57a7fdda20174a0825a329a5722dea998bfcb
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-12-19 14:38:41 -0500

    grdgr

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-Unit-6-02-JS/sw.js", {
-    scope: "/ICS20-Unit-6-02-JS/",
+  navigator.serviceWorker.register("/ICS20-6-02-JS/sw.js", {
+    scope: "/ICS20-6-02-JS/",
   })
 }
 
